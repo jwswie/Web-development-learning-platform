56c00ff60b31a0601a561fe31227961be0131c7f
N1kmax <nkmax6032@gmail.com>
2024-03-30 17:34:11 +0100

    Almost done with code-enviroment

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-document.querySelectorAll('.editor-pane textarea').forEach(textarea => {
-    textarea.addEventListener('input', render);
-});
+// document.querySelectorAll('.editor-pane textarea').forEach(textarea => {
+//     textarea.addEventListener('input', render);
+// });
+const runButton = document.getElementById('RunButton');
+const FromWindow = document.getElementById('FromWindow');
 
-function render() {
+let codeSource;
+class CodeSource{
+    JsCode;
+    HtmlCode;
+    CssCode;
+    constructor (){
+        this.JsCode = "";
+        this.HtmlCode = "";
+        this.CssCode = "";
+    }
+}
+
+function render(e) {
     var htmlCode = document.getElementById('html-editor').value;
-    var cssCode = document.getElementById('css-editor').value;
-    var jsCode = document.getElementById('js-editor').value;
+    var cssCode = codeSource.CssCode;
+    var jsCode = codeSource.JsCode;//document.getElementById('js-editor').value;
 
     var output = document.getElementById('output');
     output.innerHTML = htmlCode;
+    
 
     var style = document.createElement('style');
     style.innerHTML = cssCode;
@@ -18,3 +33,15 @@ function render() {
     script.innerHTML = jsCode;
     output.appendChild(script);
 }
+
+function startCodeEnviroment(){
+    codeSource = new CodeSource();
+}
+
+function Save() {
+    codeSource.cssCode = document.getElementById('css-editor').value;
+}
+
+startCodeEnviroment();
+FromWindow.addEventListener('click', Save);
+runButton.addEventListener("click", render);
